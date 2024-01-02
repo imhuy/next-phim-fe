ced700cb15ffebb174bfbd6a6e1a575891890a30
Andy <huylv.dev@gmail.com>
2024-01-02 17:23:57 +0700

    first commit

--- src/components/JwLoader/Jwplay.js
+++ src/components/JwLoader/Jwplay.js
@@ -5,19 +5,23 @@ import { useEffect, useRef } from 'react';
 
 const VideoPlayer = ({ video }) => {
     const playerRef = useRef(null);
- 
+
     useEffect(() => {
         if (playerRef.current && typeof window !== "undefined") {
-            console.log('windowwindowwindow', window.jwplayer);
-            window.jwplayer(playerRef.current).setup({
-                sources: [{ file: video, type: 'hls' }],
-                autostart: 'false',
-                // width: '60%',
-                // height: '20px',
-                aspectratio: '16:9',
-                playbackRateControls: 'true',
-                stretching: 'fill'
-            });
+
+            setTimeout(() => {
+                console.log('windowwindowwindow', window.jwplayer);
+                window.jwplayer(playerRef.current).setup({
+                    sources: [{ file: video, type: 'hls' }],
+                    autostart: 'false',
+                    // width: '60%',
+                    // height: '20px',
+                    aspectratio: '16:9',
+                    playbackRateControls: 'true',
+                    stretching: 'fill'
+                });
+            }, 1000);
+
         }
     }, [video]);
 
