f7712a11a1e1fe5fce6af583542d5b4721a414f6
Andy <huylv.dev@gmail.com>
2024-01-02 15:56:40 +0700

    first commit

--- src/app/play/[id]/page.tsx
+++ src/app/play/[id]/page.tsx
@@ -18,16 +18,11 @@ async function Home() {
     const header_url = headersList.get('x-url') || "";
 
     const slugUrl = header_url.replace('https://next-phim-fe.vercel.app/', '')
-    let data = await getData(slugUrl);
+    // let data = await getData(slugUrl);
 
     return (
         <div className="flex flex-wrap gap-10 container justify-center items-center mt-4">
-            {data.map((item: any, i: number) =>
-                <div key={i}>
-                    {/* <VideoPlayer video={item.link_m3u8} /> */}
-                    <span>Tập: {slugUrl}</span>
-                </div>
-            )}
+            {slugUrl}
 
         </div>
     )
